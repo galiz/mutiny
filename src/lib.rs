c862e20e768f85a3b1091940def33aa10b3d02fd
Sam Clements <sam@borntyping.co.uk>
2015-02-05 07:36:17 +0000

    Move getpid() to psutil

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,16 @@
 #![feature(io)]
-#![feature(libc)]
 #![feature(path)]
 #![feature(os)]
 
-extern crate libc;
 extern crate term;
+extern crate psutil;
 
 use std::old_io::File;
 use std::os;
 
-pub type PID = i32;
-
-/// Get the PID of the current process
-///
-/// At some point this is worth moving into rust-psutil
-fn getpid() -> PID {
-    unsafe { libc::getpid() }
-}
-
 /// Writes the PID of the current process to a file
 pub fn pidfile(path: &Path) {
-    write!(&mut File::create(path).unwrap(), "{}", getpid()).unwrap();
+    write!(&mut File::create(path).unwrap(), "{}", psutil::getpid()).unwrap();
 }
 
 /// Creates a pidfile from a name and returns it's path path
@@ -50,11 +40,10 @@ macro_rules! print_meta(
 
 /// Announce some information about the current program, and create a pidfile
 pub fn start(description: &str) {
-    let pid = getpid();
     let path = pidfile_for(os::args()[0].clone());
 
     // Announce information about the program
     print_meta!("Description", description);
     print_meta!("Pidfile", path.display());
-    print_meta!("PID", pid);
+    print_meta!("PID", psutil::getpid());
 }
