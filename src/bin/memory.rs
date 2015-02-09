bf8984a1f79875d1a2f964900e53bcb44b32da3e
Sam Clements <sam@borntyping.co.uk>
2015-02-09 14:28:13 +0000

    Slowly consume memory

--- src/bin/memory.rs
+++ src/bin/memory.rs
@@ -1,17 +1,24 @@
 #![feature(core)]
 #![feature(io)]
+#![feature(std_misc)]
 
 extern crate wrecking_ball;
 
 use std::old_io as io;
+use std::old_io::timer::Timer;
+use std::time::duration::Duration;
 
 fn main() {
     wrecking_ball::start("Consume memory and wait to be killed");
 
     let mut vec = Vec::new();
+    let mut timer = Timer::new().unwrap();
 
-    for i in 0..10000000 {
-        vec.push(i);
+    for _ in 0..20 {
+        timer.sleep(Duration::milliseconds(500));
+        for i in 0..1000000 {
+            vec.push(i);
+        }
     }
 
     for line in io::stdin().lock().lines() {
