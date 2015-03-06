499e8e29807a4abe307952ed29374924c362036a
Sam Clements <sam@borntyping.co.uk>
2015-03-06 21:14:17 +0000

    Useful iteraction output, predictable memory usage via Vec::shrink_to_fit

--- src/memory.rs
+++ src/memory.rs
@@ -2,15 +2,27 @@ use std::old_io as io;
 use std::old_io::timer::Timer;
 use std::time::duration::Duration;
 
+use psutil::getpid;
+use psutil::process::Process;
+
 pub fn start() {
-    let mut vec = Vec::new();
+    let mut vec: Vec<u64> = Vec::with_capacity(0);
     let mut timer = Timer::new().unwrap();
 
-    for _ in 0..20 {
-        timer.sleep(Duration::milliseconds(500));
-        for i in 0..1000000 {
+    for t in 0..10 {
+        // Allocate memory
+        for i in 0..5000000 {
             vec.push(i);
         }
+        vec.shrink_to_fit();
+
+        // Announce current memory usage
+        let memory = Process::new(getpid()).unwrap().memory().unwrap();
+        info!("Iteration {}: using {} Mb ({} * u64)",
+            t, memory.rss / 1024 / 1024, vec.capacity());
+
+        // Sleep so that memory is allocated over time
+        timer.sleep(Duration::seconds(1));
     }
 
     let mut stdin = io::stdin();
